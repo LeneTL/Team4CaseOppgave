7fd8be27a01c13f3e449a7abc52c4b738be1d9d2
Big Poo <156303066+SigurdHSimensen@users.noreply.github.com>
2026-02-09 14:48:14 +0100

    .

---
+++ data/database.cs
@@ -0,0 +1,11 @@
+﻿namespace CaseOppgaveTeam4.data
+{
+    public sealed class database
+    {
+        public database()
+        {
+            var dbPath = Path.Combine(AppContext.BaseDirectory, "data", "app.db");
+            var connectionString = $"Data Source={dbPath}";
+        }
+    }
+}
