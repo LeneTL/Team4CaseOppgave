0f684da9320d9c96478098e8b84b93e829a8b82a
Big Poo <156303066+SigurdHSimensen@users.noreply.github.com>
2026-02-10 09:55:54 +0100

    .

--- Program.cs
+++ Program.cs
@@ -51,78 +51,7 @@ namespace CaseOppgaveTeam4
 
             app.MapPost("/events", async (HttpRequest request) =>
             {
-                using var document = await JsonDocument.ParseAsync(request.Body);
-                var root = document.RootElement;
-
-                //validering :)
-                if (!root.TryGetProperty("eventId", out var eventIdProp))
-                    return Results.BadRequest("eventId mangler");
-                if (!Guid.TryParse(eventIdProp.GetString(), out _))
-                    return Results.BadRequest("eventId er ikke gyldig UUID");
-                if (!root.TryGetProperty("type", out var typeProp))
-                    return Results.BadRequest("type mangler");
-
-                var type = typeProp.GetString()!;
-                string? studentId = null;
-
-                if (root.TryGetProperty("studentId", out var sid)) //ikke SIDS
-                    studentId = sid.GetString();
-                if (type != "student_registrert" && studentId == null)
-                    return Results.BadRequest("studentId mangler");
-                if (type == "student_registerert")
-                    studentId = Guid.NewGuid().ToString();
-
-                var occuredUtc = root.GetProperty("occuredUtc").GetString();
-                var recordedUtc = root.GetProperty("recordedUtc").GetString();
-
-                var course = root.TryGetProperty("course", out var c) ? c.GetString() : null;
-                var year = root.TryGetProperty("year", out var y) ? y.GetInt32() : (int?)null;
-                var semester = root.TryGetProperty("semester", out var s) ? s.GetInt32() : (int?)null;
-
-                var payload = root.GetRawText();
-
-                const string sql = """
-                    INSERT OR IGNORE INTO events ( 
-                        event_id,
-                        occured_utc,
-                        recorded_utc,
-                        type,
-                        student_id,
-                        course,
-                        year,
-                        semester,
-                        payload
-                    )
-                    VALUES (
-                        @EventId,
-                        @OccuredUtc,
-                        @RecordedUtc,
-                        @Type,
-                        @StudentId,
-                        @Course,
-                        @Year,
-                        @Semester,
-                        @Payload
-                    );
-                """;
-
-                using var connection = new SqliteConnection(connectionString);
-                await connection.ExecuteAsync(sql, new
-                {
-                    EventId = eventIdProp.GetString(),
-                    OccuredUtc = occuredUtc,
-                    RecordedUtc = recordedUtc,
-                    Type = type,
-                    StudentId = studentId,
-                    Course = course,
-                    Year = year,
-                    Semester = semester,
-                    Payload = payload
-                });
-
-                if (type == "student_registrert")
-                    return Results.Ok(new { ok = true, studentId });
-                return Results.Ok(new { ok = true });
+                
             });
 
             app.MapGet("/events/count", async () =>
